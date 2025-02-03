39ce280247cfc2cabb17ddec79da45479cc84b5a
Laudier st <55962333+Laudier2@users.noreply.github.com>
2025-02-02 23:06:27 -0300

    Update convertUSDT.jsx

--- src/pages/convertUSDT.jsx
+++ src/pages/convertUSDT.jsx
@@ -28,7 +28,7 @@ export default function Converso() {
       <form>
         <div className="form-row">
           <div className="form-group col-md-6 text-primary h1">
-            <label htmlFor="inputEmail4 h1-m">
+            <label className="inputEmail">
               <img src="https://cdn.pixabay.com/photo/2017/01/07/16/55/usa-1960922_960_720.jpg" alt="img" className="img" />
             </label>
             <div className="form-group input-group">
@@ -47,7 +47,7 @@ export default function Converso() {
             </div>
           </div>
           <div className="form-group col-md-6 text-primary h1">
-            <label htmlFor="inputPassword4 h1 h1-m">
+            <label className="inputEmail">
               <img src="https://img.freepik.com/vetores-gratis/ilustracao-de-bandeira-brasil_53876-27017.jpg?size=626&ext=jpg" alt="img" className="img mt-2" />
             </label>
             <div className="form-group input-group">
