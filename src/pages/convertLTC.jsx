258eb81aed2c09b62f8c8ce75d9e59efdc974290
Laudier st <55962333+Laudier2@users.noreply.github.com>
2025-02-02 23:06:11 -0300

    Update convertLTC.jsx

--- src/pages/convertLTC.jsx
+++ src/pages/convertLTC.jsx
@@ -29,7 +29,7 @@ export default function Converso() {
       <form>
         <div className="form-row">
           <div className="form-group col-md-6 text-primary h1">
-            <label htmlFor="inputEmail4 h1-m">
+            <label className="inputEmail">
               <img src="https://media.istockphoto.com/photos/crypto-currency-gold-litecoin-on-dollar-banknote-on-black-digital-picture-id912790044?k=20&m=912790044&s=612x612&w=0&h=5kV6q-KT08deAgJ2uZwOnEqK90YLxllNQYGj25A2Ozk=" alt="img" className="img mt-2" />
             </label>
             <div className="form-group input-group">
@@ -48,7 +48,7 @@ export default function Converso() {
             </div>
           </div>
           <div className="form-group col-md-6 text-primary h1">
-            <label htmlFor="inputPassword4 h1 h1-m">
+            <label className="inputEmail">
               <img src="https://img.freepik.com/vetores-gratis/ilustracao-de-bandeira-brasil_53876-27017.jpg?size=626&ext=jpg" alt="img" className="img mt-4" />
             </label>
             <div className="form-group input-group">
